c6ca7138b1535dc25b8b470bf2af85a8fb28512f
Francesco Limpias <francescolimpias@gmail.com>
2023-01-24 16:37:49 +0100

    Created MovieLibrary class

---
+++ Models/MovieLibrary.php
@@ -0,0 +1,31 @@
+<?php
+
+class MovieLibrary
+{
+
+    public $owner;
+    public $movies;
+
+    // Constructor
+    public function __construct($owner)
+    {
+        $this->owner = $owner;
+        $this->movies = [];
+    }
+
+    // Properties
+    public function addMovie(Movie $movie)
+    {
+        $this->movies[] = $movie;
+    }
+
+    public function getMoviesByTitle($title)
+    {
+        return array_filter(
+            $this->movies,
+            function ($movie) use ($title) {
+                return $movie->title == $title;
+            }
+        );
+    }
+}
